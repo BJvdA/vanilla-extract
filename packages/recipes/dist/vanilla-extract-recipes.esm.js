549d201e3cdc232e28e36f0180ffdf8c38b2def2
Bart van den Aardweg <bartvandenaardweg@gmail.com>
2022-02-08 13:26:03 +0100

    chore: build

--- packages/recipes/dist/vanilla-extract-recipes.esm.js
+++ packages/recipes/dist/vanilla-extract-recipes.esm.js
@@ -12,6 +12,64 @@ function mapValues(input, fn) {
   return result;
 }
 
+function getStyleValues(options, styleRule, getId) {
+  if ('conditions' in options) {
+    var result = {
+      conditions: {}
+    };
+    var defaultConditions = options.defaultCondition ? Array.isArray(options.defaultCondition) ? options.defaultCondition : [options.defaultCondition] : [];
+    var defaultClasses = [];
+
+    if ('responsiveArray' in options) {
+      result.responsiveArray = options.responsiveArray;
+    }
+
+    for (var conditionName in options.conditions) {
+      var styleValue = styleRule;
+      var condition = options.conditions[conditionName];
+
+      if (condition['@supports']) {
+        styleValue = {
+          '@supports': {
+            [condition['@supports']]: styleValue
+          }
+        };
+      }
+
+      if (condition['@media']) {
+        styleValue = {
+          '@media': {
+            [condition['@media']]: styleValue
+          }
+        };
+      }
+
+      if (condition.selector) {
+        styleValue = {
+          selectors: {
+            [condition.selector]: styleValue
+          }
+        };
+      }
+
+      var className = style(styleValue, "".concat(getId(), "_").concat(conditionName));
+      result.conditions[conditionName] = className;
+
+      if (defaultConditions.indexOf(conditionName) > -1) {
+        defaultClasses.push(className);
+      }
+    }
+
+    if (defaultClasses.length > 0) {
+      result.defaultClass = defaultClasses.join(' ');
+    }
+
+    return result;
+  } else {
+    return style(styleRule, getId());
+  }
+}
+
 function recipe(options, debugId) {
   var {
     variants = {},
@@ -24,62 +82,16 @@ function recipe(options, debugId) {
   var variantClassNames = mapValues(variants, (variantGroup, variantGroupName) => {
     var styles = {};
 
-    for (var _key2 in variantGroup) {
+    var _loop = function _loop(_key2) {
       var styleRule = variantGroup[_key2];
+      styles[_key2] = getStyleValues(options, typeof styleRule === 'string' ? [styleRule] : styleRule, () => {
+        var id = "".concat(variantGroupName, "_").concat(_key2);
+        return debugId ? "".concat(debugId, "_").concat(id) : id;
+      });
+    };
 
-      if ('conditions' in options) {
-        styles[_key2] = {
-          conditions: {}
-        };
-        var defaultConditions = options.defaultCondition ? Array.isArray(options.defaultCondition) ? options.defaultCondition : [options.defaultCondition] : [];
-        var defaultClasses = [];
-
-        if ('responsiveArray' in options) {
-          styles[_key2].responsiveArray = options.responsiveArray;
-        }
-
-        for (var conditionName in options.conditions) {
-          var styleValue = typeof styleRule === 'string' ? [styleRule] : styleRule;
-          var condition = options.conditions[conditionName];
-
-          if (condition['@supports']) {
-            styleValue = {
-              '@supports': {
-                [condition['@supports']]: styleValue
-              }
-            };
-          }
-
-          if (condition['@media']) {
-            styleValue = {
-              '@media': {
-                [condition['@media']]: styleValue
-              }
-            };
-          }
-
-          if (condition.selector) {
-            styleValue = {
-              selectors: {
-                [condition.selector]: styleValue
-              }
-            };
-          }
-
-          var className = style(styleValue, "".concat(variantGroupName, "_").concat(_key2, "_").concat(conditionName));
-          styles[_key2].conditions[conditionName] = className;
-
-          if (defaultConditions.indexOf(conditionName) > -1) {
-            defaultClasses.push(className);
-          }
-        }
-
-        if (defaultClasses.length > 0) {
-          styles[_key2].defaultClass = defaultClasses.join(' ');
-        }
-      } else {
-        styles[_key2] = style(typeof styleRule === 'string' ? [styleRule] : styleRule, debugId ? "".concat(debugId, "_").concat(variantGroupName, "_").concat(_key2) : "".concat(variantGroupName, "_").concat(_key2));
-      }
+    for (var _key2 in variantGroup) {
+      _loop(_key2);
     }
 
     return styles;
@@ -90,7 +102,10 @@ function recipe(options, debugId) {
     style: theStyle,
     variants: _variants
   } of compoundVariants) {
-    compounds.push([_variants, typeof theStyle === 'string' ? theStyle : style(theStyle, "".concat(debugId, "_compound_").concat(compounds.length))]);
+    compounds.push([_variants, getStyleValues(options, typeof theStyle === 'string' ? [theStyle] : theStyle, () => {
+      var id = "compound_".concat(compounds.length);
+      return debugId ? "".concat(debugId, "_").concat(id) : id;
+    })]);
   }
 
   var config = {
